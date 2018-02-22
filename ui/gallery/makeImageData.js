724f72c83a1f96a4a18dbea3c3cb2ee8588bf4c8
sosim13 <sosim13p@gmail.com>
2018-02-22 15:38:09 +0900

    321

--- ui/gallery/makeImageData.js
+++ ui/gallery/makeImageData.js
@@ -122,3 +122,46 @@ function setStatusText(msg)
 
 makeImageData();
 
+
+
+// 2014/01/08 에 서버 페이지가 변경됨
+
+?function hideElement(id)
+{
+	var element = document.getElementById(id);
+
+	if (element)
+  		element.style.display='none';
+}
+
+function hideElementByClass(className)
+{
+  	var tags=document.getElementsByClassName(className);
+  	
+	for (var i = 0; i < tags.length; ++i) {
+  		var element = tags[i];
+  		element.style.display='none';
+  	}		
+}
+
+function css(selector, property, value) {
+    for (var i=0; i < document.styleSheets.length; ++i) {
+        try { 
+        	document.styleSheets[i].insertRule(selector+ ' {'+property+':'+value+'}', document.styleSheets[i].cssRules.length);
+        } catch(err) {
+        }
+    }
+}
+
+hideElement('intro');
+hideElement('gads');
+hideElement('movepage');
+hideElement('banner1');
+hideElement('footer');
+
+css("div", "display", "none");
+
+
+window.setTimeout("agate.runScript('caller.showNow')", 500);
+
+

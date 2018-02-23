7cc2f8578997ff8a45fda02c4c0b48c7c48c4bc6
sosim13 <sosim13p@gmail.com>
2018-02-24 00:11:45 +0900

    321

--- ui/js/common.js
+++ ui/js/common.js
@@ -38,7 +38,7 @@ function getMomlAd()
 	if (sc.length == 0)
 		return;
 
-	var momlAd = '<div style="width: 100%;"><div class="adBar" id="adBar" style="padding-top: 13%;" ><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-7252630941302629" data-ad-slot="4292477114"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></div></div>';
+	var momlAd = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-7252630941302629" data-ad-slot="4292477114"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
 	return momlAd; 
 }
 

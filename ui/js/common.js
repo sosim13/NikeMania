54f707ac7b90265e14a7b540a944cd86ce5fafff
sosim13 <sosim13p@gmail.com>
2018-02-24 00:10:35 +0900

    321

--- ui/js/common.js
+++ ui/js/common.js
@@ -34,6 +34,10 @@ function insertAdBefore(html)
 
 function getMomlAd()
 {
+	var sc = document.getElementById("banner_box");
+	if (sc.length == 0)
+		return;
+
 	var momlAd = '<div style="width: 100%;"><div class="adBar" id="adBar" style="padding-top: 13%;" ><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-7252630941302629" data-ad-slot="4292477114"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></div></div>';
 	return momlAd; 
 }

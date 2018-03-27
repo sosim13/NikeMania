f73127552fb009f296de7416ad085bf211aa1217
sosim13 <sosim13p@gmail.com>
2018-03-27 13:13:49 +0900

    321

--- ui/js/common.js
+++ ui/js/common.js
@@ -117,7 +117,7 @@ css("div#con_head1", "width", "1000px");
 css("div#con_head1", "height", "52px");
 css("div#con_tail", "width", "1000px");
 css("a:link", "font-size", "25pt");
-css("div#r_idx_r img", "width", "50px");
+css("img", "width", "5px");
 
 
 
